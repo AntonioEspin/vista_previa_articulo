2bab58125bf5908a09af0c474b95b4e0a890f441
Antonio Espin <antoesdev9@gmail.com>
2020-08-28 17:06:11 -0500

    add javascript

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let share = document.getElementById('image-share');
+
+share.addEventListener('click', () => {
+    let navShare = document.getElementById('nav-share');
+    let setDisplay = navShare.style.display = 'flex'
+    return setDisplay
+})
